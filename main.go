266bfb5e2c7ff1700b666d61833092ca4cfe44b3
robotrainer <asbu26@gmail.com>
2021-09-04 01:41:27 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"strings"
+)
 
 func main() {
-	fmt.Println("mtdlBot")
+	bot, err := tgbotapi.NewBotAPI("")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		word := strings.Fields(update.Message.Text)
+		fmt.Println(word)
+
+		switch word[0] {
+		case "ADD":
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Команда ADD"))
+		case "RM":
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Команда RM"))
+		case "TG":
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Команда TG"))
+		case "CL":
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Команда CL"))
+		case "ALL":
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Команда ALL"))
+		default:
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неизвестная команда"))
+		}
+	}
 }
